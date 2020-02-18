51569bd438675c7a69035692fd385d611b413a5a
Marija Kardum <marijakardum1@gmail.com>
2020-02-18 17:13:39 +0100

    Implementiran search

--- eat_out_project/src/pages/split.js
+++ eat_out_project/src/pages/split.js
@@ -9,11 +9,15 @@ import { Breadcrumb, BreadcrumbItem, CustomInput,Form,FormGroup,Label, Input,Inp
 
 class Split extends React.Component {
   state = {
-    inputText: '' 
+    inputText: ''
   }
 
   handleInput = (e) => {
     this.setState({inputText: e.target.value});
+  }
+
+  handleSubmit = () => {
+    this.forceUpdate();
   }
 
   render() {
@@ -47,7 +51,7 @@ class Split extends React.Component {
                     
                     </Input>
                     <InputGroupAddon addonType="append">
-                      <Button onClick={() => console.log("KLIKNUTO: "+ this.state.inputText)}>
+                      <Button onClick={this.handleSubmit}>
                         <i class="fa fa-search"></i>
                       </Button>
                     </InputGroupAddon>
@@ -83,25 +87,41 @@ class Split extends React.Component {
             </Col>
           <Col md="10" id="Split" style={{backgroundColor:'white'}}>
             <StaticQuery
-              query= {blogQuery1} 
+              query={blogQuery1}
               render={data => {
-              return (
-                <div class="card-columns" >  
-                {data.allMarkdownRemark.edges.map(({ node }) => (
-                  <Restoran
-                  key={node.id}
-                  title={node.frontmatter.title}  
-                  slug={node.fields.slug}
-                  body={node.excerpt}
-                  fluid={node.frontmatter.image.childImageSharp.fluid}
-                  tags={node.frontmatter.tags}
-                  type={node.frontmatter.type}
-                />
-                ))}
-                
-                
-                </div>
-              )
+                return (
+                  <div class="card-columns" >  
+                    {data.allMarkdownRemark.edges.map(({ node }) => {
+                      if (this.state.inputText !== '') {
+                        let inputText = this.state.inputText
+                        let restaurantTitle = node.frontmatter.title
+                        if (restaurantTitle.toLowerCase().includes(inputText.toLowerCase())) {
+                          return (
+                            <Restoran
+                              key={node.id}
+                              title={node.frontmatter.title}  
+                              slug={node.fields.slug}
+                              body={node.excerpt}
+                              fluid={node.frontmatter.image.childImageSharp.fluid}
+                              tags={node.frontmatter.tags}
+                              type={node.frontmatter.type}
+                            />
+                          )}
+                      } else {
+                        return (
+                          <Restoran
+                            key={node.id}
+                            title={node.frontmatter.title}  
+                            slug={node.fields.slug}
+                            body={node.excerpt}
+                            fluid={node.frontmatter.image.childImageSharp.fluid}
+                            tags={node.frontmatter.tags}
+                            type={node.frontmatter.type}
+                          />
+                      )}
+                    })}
+                  </div>
+                )
               }}
             />
           </Col> 
@@ -110,31 +130,31 @@ class Split extends React.Component {
       </Layout>
   )}}
 const blogQuery1 = graphql`
-query blogQuery1{
-  allMarkdownRemark(filter: {frontmatter: {type: {eq: "restoranST"}}}){
-  edges{
-    node{
-      id
-      frontmatter{
-        title
-        tags
-        type
-        image{
-          childImageSharp{
-            fluid(maxWidth: 600){
-              ...GatsbyImageSharpFluid
+  query blogQuery1{
+    allMarkdownRemark(filter: {frontmatter: {type: {eq: "restoranST"}}}){
+    edges{
+      node{
+        id
+        frontmatter{
+          title
+          tags
+          type
+          image{
+            childImageSharp{
+              fluid(maxWidth: 600){
+                ...GatsbyImageSharpFluid
+              }
             }
-          }
-        }    
+          }    
+        }
+        fields{
+          slug
+        }
+        excerpt
       }
-      fields{
-        slug
-      }
-      excerpt
     }
   }
- }
-}
+  }
 `
 
 export default Split
